ec134bcafc334540610dcacde5709fdf960cc09a
SeedPaths-User <lessdismissive@email.com>
2014-12-15 11:27:12 -0700

    Week 6 Code Challenge
    Website with ajax

---
+++ Week6CodeChallenge/Week6CodeChallenge/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week6CodeChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            else { return View(); }
+        }
+        public ActionResult About()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            else { return View(); }
+        }
+        public ActionResult Work()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            else { return View(); }
+        }
+        public ActionResult Careers()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            else { return View(); }
+        }
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            Models.Week6ContactForm contactForm = new Models.Week6ContactForm();
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Contact(Models.Week6ContactForm contactForm)
+        {
+            Models.sp5JackieEntities db = new Models.sp5JackieEntities();
+            db.Week6ContactForm.Add(contactForm);
+            db.SaveChanges();
+            return Content("Thanks!");
+        }
+        public ActionResult Who()
+        {
+            return PartialView();
+        }
+        public ActionResult What()
+        {
+            return PartialView();
+        }
+        public ActionResult When()
+        {
+            return PartialView();
+        }
+        public ActionResult Where()
+        {
+            return PartialView();
+        }
+        public ActionResult How()
+        {
+            return PartialView();
+        }
+    }
+    }
---
+++ Week6CodeChallenge/Week6CodeChallenge/Models/ContactForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Week6CodeChallenge.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required,DataType(DataType.MultilineText)]
+        public string ProjectDescription { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, DataType(DataType.MultilineText)]
+        public string Comment { get; set; }
+
+    }
+}
